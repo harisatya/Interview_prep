f12bfeb9db8ee74a0cce83c4efafb58cd1f6436b
harisatya <chundruhari@gmail.com>
2023-05-13 11:29:09 +0530

    generator function

---
+++ 8_Generator.js
@@ -0,0 +1,31 @@
+//Generator Function
+// in side prototype we have
+//--> Next() --> next allows to execute inside function
+//--> throw()
+//--> return()
+// any generator will have value and done property done 
+//will not be set to true when all code in generator got completed
+// Usecase we can create infinite loop
+function* sampleGenerator(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+const genobj = sampleGenerator();
+const obj = genobj.next();
+ console.log(obj);
+
+
+ // gen function we cna write infinite times with generators
+ function* genID(){
+    let id = 0;
+    while(true){
+        yield id;
+        id++
+    }
+ }
+ const a = genID();
+ console.log(a.next());
+ console.log(a.next());
+ console.log(a.next());
+ console.log(a.next());
